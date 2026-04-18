02353f14c7d49f4f0205c3d049e382662c9837e7
gprashant-src <gprashant.code@gmail.com>
2026-04-18 21:48:50 +0530

    feat: added histogram equalization implement.

--- Histogram/equalization.py
+++ Histogram/equalization.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from collections import defaultdict
 
 img = cv2.imread("data/test1.jpg", cv2.IMREAD_GRAYSCALE)
 h, w = img.shape
@@ -13,13 +14,30 @@ h, w = img.shape
 hist = np.bincount(img.flatten(), minlength=256)
 
 """For normalized histogram"""
-# hist = hist / (h * w)
+hist = hist / (h * w)
 
-cv2.imshow("Image", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+T_r = np.round(np.cumsum(hist) * 255).astype(int)
 
-sns.barplot(x=range(256), y=hist)
-plt.xticks([])
+H = defaultdict(list)
+for r, t_r in enumerate(T_r):
+    H[t_r].append(hist[r])
+
+res = np.array([sum(H[i]) for i in range(256)])
+
+# cv2.imshow("Image", img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+fig, axes = plt.subplots(1, 2, figsize=(12, 4))
+
+sns.barplot(x=range(256), y=hist, ax=axes[0])
+axes[0].set_title("Before Equalization")
+axes[0].set_xticks([])
+
+
+sns.barplot(x=range(256), y=res, ax=axes[1])
+axes[1].set_title("After Equalization")
+axes[1].set_xticks([])
+
+plt.tight_layout()
 plt.show()
-
