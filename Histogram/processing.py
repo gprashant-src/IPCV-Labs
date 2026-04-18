5dc97d8bd3be6445778f3a8b05a7a34fd8800792
gprashant-src <gprashant.code@gmail.com>
2026-04-18 20:52:38 +0530

    fix:changes

--- Histogram/processing.py
+++ Histogram/processing.py
@@ -6,13 +6,18 @@ import matplotlib.pyplot as plt
 img = cv2.imread("data/test1.jpg", cv2.IMREAD_GRAYSCALE)
 h, w = img.shape
 
-hist = np.zeros(256)
+# hist = np.zeros(256)
+# for p in img.flatten():
+#     hist[p] += 1
 
-for p in img.flatten():
-    hist[p] += 1
+hist = np.bincount(img.flatten(), minlength=256)
+
+"""For normalized histogram"""
+# hist = hist / (h * w)
 
 cv2.imshow("Image", img)
 cv2.waitKey(0)
+
 cv2.destroyAllWindows()
 
 sns.barplot(x=range(256), y=hist)
