c9731cc9658758a068c4495933be1f08959e12e7
gprashant-src <gprashant.code@gmail.com>
2026-04-18 21:59:27 +0530

    refactor: numpy use of mapping of T_r with hist

--- Histogram/equalization.py
+++ Histogram/equalization.py
@@ -7,35 +7,27 @@ from collections import defaultdict
 img = cv2.imread("data/test1.jpg", cv2.IMREAD_GRAYSCALE)
 h, w = img.shape
 
-# hist = np.zeros(256)
-# for p in img.flatten():
-#     hist[p] += 1
-
 hist = np.bincount(img.flatten(), minlength=256)
 
 """For normalized histogram"""
-hist = hist / (h * w)
+hist_1 = hist / (h * w)
+T_r = np.round(np.cumsum(hist_1) * 255).astype(int)
 
-T_r = np.round(np.cumsum(hist) * 255).astype(int)
+# H = defaultdict(int)
+# for r, t_r in enumerate(T_r):
+#     H[t_r] += hist[r]
+# res = np.array([H[i] for i in range(256)])
 
-H = defaultdict(list)
-for r, t_r in enumerate(T_r):
-    H[t_r].append(hist[r])
-
-res = np.array([sum(H[i]) for i in range(256)])
-
-# cv2.imshow("Image", img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+res = np.zeros(256, dtype=np.uint64)
+np.add.at(res, T_r, hist)
 
 fig, axes = plt.subplots(1, 2, figsize=(12, 4))
 
-sns.barplot(x=range(256), y=hist, ax=axes[0])
+sns.barplot(x=range(256), y=hist, ax=axes[0], color="blue")
 axes[0].set_title("Before Equalization")
 axes[0].set_xticks([])
 
-
-sns.barplot(x=range(256), y=res, ax=axes[1])
+sns.barplot(x=range(256), y=res, ax=axes[1], color="orange")
 axes[1].set_title("After Equalization")
 axes[1].set_xticks([])
 
