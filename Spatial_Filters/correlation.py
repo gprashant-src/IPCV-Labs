add: gaussian blur normal implement.. fix: precision errors in correlation by clipping and floating point precisions

Spatial_Filters/correlation.py
@@ -9,14 +9,15 @@ def filter(img:np.ndarray, kernel:np.ndarray):
     ph, pw = kh // 2, kw // 2
 
     padded = np.pad(img, ((ph, ph), (pw, pw)), mode="constant")
-    output = np.zeros_like(img)
+    output = np.zeros_like(img, dtype=np.float64)
 
     for i in range(h):
         for j in range(w):
             reg = padded[i:i + kh, j:j + kw]
             output[i, j] = np.sum(reg * kernel)
     
-    return output
+    output = np.clip(output, 0, 255)
+    return output.astype(dtype=np.uint8)
 
 img = np.array([
     [1, 2, 3],